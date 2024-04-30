cfe8e6b804dde225ee44e2e5411fad626ab49303
galangbuana <220040092@stikom-bali.ac.id>
2024-05-01 02:16:20 +0800

    updating html tags

--- public/index.php
+++ public/index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>XYZ Sales API End Point</title>
 </head>
 <body>
-    <h1>XYZ Sales API End Point</h1>
+    <h1>
+        UJIAN TENGAH SEMESTER<br>Back-End Web Develompent
+    </h1>
 </body>
 </html>
